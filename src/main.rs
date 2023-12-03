aee72e29883af803b22a7f0c81b9c2f6ee8088bd
dnabre <dnabre@happybunnylabs.com>
2023-12-03 07:42:58 -0700

    day 2 start

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+#![allow(dead_code)]
+#![allow(unused_assignments)]
+
+
 /*
-    Advent of Code 2023: Day 01
-        part1 answer:   54630
-        part2 answer:   54770
+    Advent of Code 2023: Day 02
+        part1 answer:
+        part2 answer:
 
  */
 
@@ -11,15 +18,15 @@ use std::io::{BufRead, BufReader};
 const ANSWER: (&str, &str) = ("54630", "54770");
 
 fn main() {
-    let _filename_test  = "data/day01/test_input_01.txt";
-    let _filename_test2 = "data/day01/test_input_02.txt";
-    let _filename_test3 = "data/day01/test_input_03.txt";
+    let filename_test  = "data/day02/test_input_01.txt";
+    let filename_test2 = "data/day02/test_input_02.txt";
+    let filename_test3 = "data/day02/test_input_03.txt";
 
-    let filename_part1 = "data/day01/part1_input.txt";
-    let filename_part2 = "data/day01/part2_input.txt";
+    let filename_part1 = "data/day02/part1_input.txt";
+    let filename_part2 = "data/day02/part2_input.txt";
 
-    let answer1: String = part1(filename_part1);
-    let answer2: String = part2(filename_part2);
+    let answer1: String = part1(filename_test);
+    let answer2: String = part2(filename_test);
 
     println!("Advent of Code, Day 01");
     println!("    ---------------------------------------------");
@@ -46,76 +53,14 @@ fn file_to_lines(input_file: &str) -> Vec<String> {
 
 fn part1(input_file: &str) -> String {
     let lines = file_to_lines(input_file);
-    let mut sum = 0;
 
-    for l in lines.iter() {
-        let line_answer = solve_line(l);
-        sum += line_answer;
-    }
-    return sum.to_string();
+    return String::new();
 }
 
 
 fn part2(input_file: &str) -> String {
     let lines = file_to_lines(input_file);
-    let mut sum = 0;
 
-    for line_number in 0..lines.len() {
-        let l = &lines[line_number];
-        let line_value = solve_line2(&l);
-        sum += line_value;
-    }
-
-    return sum.to_string();
+    return String::new();
 }
 
-
-fn solve_line(input_line: &String) -> i32 {
-    let letters: Vec<char> = input_line.chars().collect();
-    let mut first: Option<i32> = None;
-    let mut back: Option<i32> = None;
-
-    for c in letters.iter() {
-        if c.is_digit(10) {
-            let c_v = (*c as i32) - ('0' as i32);
-            if first.is_none() {
-                first = Some(c_v);
-            }
-            back = Some(c_v);
-        }
-    }
-    return (10 * first.unwrap()) + back.unwrap();
-}
-
-fn solve_line2(input: &String) -> usize {
-    let words = vec![
-        "1", "2", "3", "4", "5", "6", "7", "8", "9",
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-
-    let mut forward_values: Vec<_> = Vec::new();
-    let mut backward_values: Vec<_> = Vec::new();
-
-    for i in 0..words.len() {
-        let p = (i, words[i]);
-        match input.find(p.1).map(|pos| (pos, p.0)) {
-            None => {}
-            Some(ff) => {
-                forward_values.push(ff);
-            }
-        }
-        match input.rfind(p.1).map(|pos| (pos, p.0)) {
-            None => {}
-            Some(bb) => {
-                backward_values.push(bb);
-            }
-        }
-    }
-
-    let f = forward_values.iter().min().unwrap().1 as i32;
-    let b = backward_values.iter().max().unwrap().1 as i32;
-
-    // first for zero-based index
-    let f = f % 9 + 1;
-    let b = b % 9 + 1;
-    return ((f * 10) + b) as usize;
-}
