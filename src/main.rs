5f0ed3d68691ee27436155c5b357167e1d595c1e
dnabre <dnabre@happybunnylabs.com>
2023-12-05 06:45:32 -0700

    day 4 part 1 done, clean to start with

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::Instant;
 
-const ANSWER: (&str, &str) = ("527369", "73074886");
+const ANSWER: (&str, &str) = ("18653", "73074886");
 
 fn main() {
     let _filename_test = "data/day04/test_input_01.txt";
@@ -26,23 +26,23 @@ fn main() {
     let filename_part2 = "data/day04/part2_input.txt";
 
     let start1 = Instant::now();
-    let answer1 = part1(_filename_test);
+    let answer1 = part1(filename_part1);
     let duration1 = start1.elapsed();
 
     let start2 = Instant::now();
     let answer2 = part2(_filename_test2);
     let duration2 = start2.elapsed();
-
-    println!("Advent of Code, Day 04");
+    //
+    // println!("Advent of Code, Day 04");
     println!("    ---------------------------------------------");
     println!("\t Part 1: {} \t\t time: {:?}", answer1, duration1);
     if ANSWER.0 != answer1 {
         println!("\t\t ERROR: Answer is WRONG. Got: {}, Expected {}", answer1, ANSWER.0);
     }
-    println!("\t Part 2: {} \t\t time: {:?}", answer2, duration2);
-    if ANSWER.1 != answer2 {
-        println!("\t\t ERROR: Answer is WRONG. Got: {}, Expected {}", answer2, ANSWER.1);
-    }
+    // println!("\t Part 2: {} \t\t time: {:?}", answer2, duration2);
+    // if ANSWER.1 != answer2 {
+    //     println!("\t\t ERROR: Answer is WRONG. Got: {}, Expected {}", answer2, ANSWER.1);
+    // }
     println!("    ---------------------------------------------");
 }
 
@@ -58,8 +58,13 @@ fn file_to_lines(input_file: &str) -> Vec<String> {
 fn part1(input_file: &str) -> String {
     let lines = file_to_lines(input_file);
 
+    let mut total_score = 0;
+    for l in &lines {
+        let score = score_line(l);
+        total_score += score;
+    }
 
-    return String::new();
+    return total_score.to_string();
 }
 
 
@@ -70,4 +75,31 @@ fn part2(input_file: &str) -> String {
     return String::new();
 }
 
+
+fn score_line(input_line:&str) -> i32 {
+    let (_,numbers) = input_line.split_once(":").unwrap();
+    let (s_winning,s_ours) = numbers.split_once("|").unwrap();
+
+    let n_wins: HashSet<i32> =
+        HashSet::from_iter(parse_number_list_whitespace(s_winning));
+    let n_ours = parse_number_list_whitespace(s_ours);
+
+    let mut wins = 0;
+    for n in n_ours {
+        if n_wins.contains(&n) {
+            wins += 1;
+        }
+    }
+    static BASE: i32 = 2;
+    if wins == 0 {
+        return 0;
+    } else
+    {
+        return BASE.pow(wins - 1);
+    }
+}
+
+fn parse_number_list_whitespace(number_string: &str) -> Vec<i32> {
+    number_string.split_whitespace().map(|s| s.trim().parse().unwrap()).collect()
+}
 
