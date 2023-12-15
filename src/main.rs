24f3711bd7c87f1c6c333b08a06ce8917bd1a2c5
dnabre <dnabre@happybunnylabs.com>
2023-12-15 13:45:43 -0700

    day 15, part 1&2 working clean up needed

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@
 /*
     Advent of Code 2023: Day 15
         part1 answer:   521341
-        part2 answer:
+        part2 answer:   252782
 
  */
 
@@ -20,7 +20,7 @@ use std::time::Instant;
 
 use advent_2023::file_to_lines;
 
-const ANSWER: (&str, &str) = ("521341", "32312");
+const ANSWER: (&str, &str) = ("521341", "252782");
 
 fn main() {
     let _filename_test1 = "data/day15/test_input_01.txt";
@@ -34,7 +34,7 @@ fn main() {
     let duration1 = start1.elapsed();
 
     let start2 = Instant::now();
-    let answer2 = part2(_filename_test2);
+    let answer2 = part2(filename_part2);
     let duration2 = start2.elapsed();
 
   //  println!("Advent of Code, Day 15");
@@ -45,10 +45,10 @@ fn main() {
         println!("\t\t ERROR: Answer is WRONG. Got: {}, Expected {}", answer1, ANSWER.0);
     }
 
-    // println!("\t Part 2: {:14} time: {:?}", answer2, duration2);
-    // if ANSWER.1 != answer2 {
-    //     println!("\t\t ERROR: Answer is WRONG. Got: {}, Expected {}", answer2, ANSWER.1);
-    // }
+    println!("\t Part 2: {:14} time: {:?}", answer2, duration2);
+    if ANSWER.1 != answer2 {
+        println!("\t\t ERROR: Answer is WRONG. Got: {}, Expected {}", answer2, ANSWER.1);
+    }
     println!("    ---------------------------------------------");
 }
 
@@ -102,9 +102,9 @@ fn part2(input_file: &str) -> String {
     let l = advent_2023::file_to_single_line(input_file,None);
     let parts = l.split(",").collect::<Vec<_>>();
     //let mut table: [Vec<u32>; 256] = [Vec::new(); 256];
-    let mut table: Vec<VecDeque<(String,u32)>>  = Vec::with_capacity(256);
+    let mut table: Vec<Vec<(String,u32)>>  = Vec::with_capacity(256);
     for i in 0..256 {
-        table.push(VecDeque::new());
+        table.push(Vec::new());
     }
 
     for p in &parts {
@@ -131,22 +131,40 @@ fn part2(input_file: &str) -> String {
             }
             num = Some(sb.parse().unwrap());
         }
-      //
+      //let entry = (key,num.unwrap());
+        //table[h_idx].push(entry);
         match op {
             '='=> {
                 let entry = (key,num.unwrap());
-                table[h_idx].push_back(entry);
-            },
-            '-' => {
-                println!("\tSearching table {h_idx} for key: {key2}");
-                println!("\t\t {:?}", table[h_idx]);
-                let mut t_idx = 0;
+                // println!("\tSearching table {h_idx} for key: {key2}");
+                // println!("\t\t {:?}", table[h_idx]);
+                let mut replaced = false;
                 for t_idx in 0..table[h_idx].len() {
                     let (s,v) = &table[h_idx][t_idx];
-                    println!("\t\t {t_idx:3} {:?}", (s,v));
+                    // println!("\t\t checking table @ {t_idx:3} {:?}", (s,v));
+                    if *s == key2 {
+                        let r=table[h_idx].remove(t_idx);
+                        table[h_idx].insert(t_idx, entry);
+                        replaced = true;
+                        // println!("\t\t removal result: {:?}", r);
+                        break;
+                    }
+                }
+                if !replaced {
+                    table[h_idx].push((key2,num.unwrap()));
+                }
+
+
+            },
+            '-' => {
+                // println!("\tSearching table {h_idx} for key: {key2}");
+                // println!("\t\t {:?}", table[h_idx]);
+                for t_idx in 0..table[h_idx].len() {
+                    let (s,v) = &table[h_idx][t_idx];
+                    // println!("\t\t checking table @ {t_idx:3} {:?}", (s,v));
                     if *s == key {
-                        let r=table[h_idx].remove(i-1);
-                        println!("\t\t removal result: {:?}", r);
+                      let r=table[h_idx].remove(t_idx);
+                        // println!("\t\t removal result: {:?}", r);
                         break;
                     }
                 }
@@ -154,20 +172,30 @@ fn part2(input_file: &str) -> String {
             }
             x =>{ panic!("unknown op: {x}")}
         }
-        println!("After \"{}\"", p);
-        print_table(&table);
-        println!("\t key: {key2}, op: {op} , num:{:?}, hash:{}", num, h);
-        println!();
+        // println!("After \"{}\"", p);
+        // print_table(&table);
+        //println!("\t key: {key2}, op: {op} , num:{:?}, hash:{}", num, h);
+        // println!();
+    }
+
+    let mut answer:usize = 0;
+    for b in 0..table.len() {
+        let v = &table[b];
+        if !v.is_empty() {
+            for i in 0..v.len() {
+                let e@(k,v) = &v[i];
+                let f_value:usize = (b+1) * (i+1) * (*v as usize);
+                answer += f_value;
+            }
+        }
     }
 
 
 
-
-    let answer = 0;
     return answer.to_string();
 }
 
-fn print_table(table: &Vec<VecDeque<(String, u32)>>) {
+fn print_table(table: &Vec<Vec<(String, u32)>>) {
     for t_idx in 0..table.len() {
         if table[t_idx].is_empty() {
             continue;
