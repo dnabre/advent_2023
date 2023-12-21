74c8ea726cf6816dde5efa1841029ef5befc5772
dnabre <dnabre@happybunnylabs.com>
2023-12-21 08:13:13 -0700

    day21 part 1 working

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,21 @@
 #![allow(unused_assignments)]
 #![allow(unreachable_code)]
 
+use std::collections::{HashSet, VecDeque};
 use std::fmt::{Display, Formatter};
+use std::mem::needs_drop;
 use std::time::Instant;
+
 
 /*
     Advent of Code 2023: Day 21
-        part1 answer:
+        part1 answer:   3642
         part2 answer:
 
 
  */
 
-const ANSWER: (&str, &str) = ("386787", "131029523269531");
+const ANSWER: (&str, &str) = ("3642", "131029523269531");
 
 
 fn main() {
@@ -31,7 +34,7 @@ fn main() {
     let duration1 = start1.elapsed();
 
     let start2 = Instant::now();
-    let answer2 = part2(filename_part2);
+    let answer2 = part2(_filename_test2);
     let duration2 = start2.elapsed();
 
     //println!("Advent of Code, Day 21");
@@ -49,10 +52,88 @@ fn main() {
     println!("    ---------------------------------------------");
 }
 
+const BLOCK_SQUARE:char = '#';
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Coord {
+     x: usize   ,
+     y: usize
+}
+
+impl Coord {
+     fn clear_at(&self, grid: &Vec<Vec<char>>) -> bool {
+        let ch = grid[self.y][self.x];
+         return ch != BLOCK_SQUARE;
+    }
+
+   fn offset(&self, dx:i32, dy:i32, max_x:usize, max_y:usize) -> Option<Coord>{
+       let (cx,cy) = (self.x,self.y);
+       let (nx, ny) = (dx + cx as i32, dy + cy as i32);
+       if (nx < 0) || (ny < 0) || (nx as usize  >= max_x ) || ( ny as usize >= max_y) {
+           return None;
+       }
+       let r = Coord {x: nx as usize, y: ny as usize};
+       return Some(r);
+    }
+}
+
+
+
 fn part1(input_file: &str) -> String {
     let lines = advent_2023::file_to_lines(input_file);
 
-    let answer =0;
+    let mut grid = advent_2023::parse_grid(&lines);
+
+    let mut start = Coord{
+        x: 0,
+        y: 0,
+    };
+
+    let max_y = lines.len();
+    let max_x = lines[0].len();
+   'y_loop: for y in 0..max_y {
+        for x in 0..max_x {
+            let ch = grid[y][x];
+            if ch == 'S' {
+                (start.x, start.y) = (x,y);
+                break 'y_loop;
+            }
+        }
+    }
+    println!("start@ {:?}", start);
+    grid[start.y][start.x] = '.';
+
+
+    let mut queue:VecDeque<(Coord,u32)> = VecDeque::new();
+    queue.push_front((start,0));
+
+    let target_steps = 64;
+    let mut target_points:HashSet<Coord> = HashSet::new();
+    let mut visited :HashSet<(Coord,u32)>= HashSet::new();
+
+    while let Some((pos,step_count)) = queue.pop_front(){
+        if step_count == target_steps {
+            target_points.insert(pos);
+            grid[pos.y][pos.x] = 'O';
+        } else {
+            for (i_x, i_y) in advent_2023::CARD_DELTA {
+                let n_coord = pos.offset(i_x, i_y, max_x, max_y);
+                if let Some(n_coord) = n_coord {
+                    let pair = (n_coord,step_count+1);
+                    if n_coord.clear_at(&grid) && !visited.contains(&(pair)){
+                        queue.push_front((n_coord, step_count + 1));
+                        visited.insert(pair);
+                    }
+                }
+            }
+        }
+
+    }
+
+  //  advent_2023::print_grid(&grid);
+
+
+    let answer = target_points.len();
     return answer.to_string();
 }
 
