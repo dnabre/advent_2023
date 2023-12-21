f0fae54e9e2fe37c1755ff9ae8c5ca91dc982614
dnabre <dnabre@happybunnylabs.com>
2023-12-21 05:23:37 -0700

    day 19, part 1 works (messy output)

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,25 @@
 #![allow(unused_assignments)]
 #![allow(unreachable_code)]
 
+use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
+use std::path::Component::ParentDir;
+use std::thread::current;
+use std::time::Instant;
+
+
 /*
-    Advent of Code 2023: Day 18
-        part1 answer:   62365
-        part2 answer:   159485361249806
+    Advent of Code 2023: Day 19
+        part1 answer:   386787
+        part2 answer:
 
 
  */
 
+const ANSWER: (&str, &str) = ("386787", "159485361249806");
 
-use std::collections::HashMap;
-use std::fmt::{Display, Formatter};
-use std::path::Component::ParentDir;
 
-use std::time::Instant;
 
-const ANSWER: (&str, &str) = ("62365", "159485361249806");
 
 fn main() {
     let _filename_test = "data/day19/test_input_01.txt";
@@ -30,7 +33,7 @@ fn main() {
     let filename_part2 = "data/day19/part2_input.txt";
 
     let start1 = Instant::now();
-    let answer1 = part1(_filename_test);
+    let answer1 = part1(filename_part1);
     let duration1 = start1.elapsed();
 
     let start2 = Instant::now();
@@ -59,6 +62,13 @@ struct Part {
     a: i64,
     s: i64,
 }
+
+impl Display for Part {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[x: {:5}, m: {:5}, a: {:5}, s: {:5}]", self.x, self.m, self.a, self.s)
+    }
+}
+
 // px{a<2006:qkq,m>2090:A,rfg}
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 struct Rule{
@@ -76,7 +86,7 @@ impl Display for Rule {
 struct RulePart {
     xmas_letter:char,
     op: char,
-    num: i32,
+    num: i64,
     queue_name: String
 }
 impl Display for RulePart {
@@ -96,63 +106,6 @@ fn xmas_to_index(ch:char) -> usize {
 }
 
 
-fn part1(input_file: &str) -> String {
-    let lines = advent_2023::file_to_lines(input_file);
-
-
-    let mut rule_list:Vec<Rule> = Vec::new();
-    let mut index: usize = 0;
-    println!("rules:");
-
-    while lines[index] != "" {
-        println!("{}", lines[index]);
-        let new_rule:Rule = parse_line(&lines[index]);
-        println!("{index:3}: {}", new_rule);
-        rule_list.push(new_rule);
-        index += 1;
-    }
-    index += 1;
-    let mut parts_list: Vec<Part> = Vec::new();
-
-
-    while index < lines.len() {
-        let p = parse_xmas(&lines[index]);
-        parts_list.push(p);
-        index += 1;
-    }
-    let parts_list = parts_list;
-
-    let mut rule_map: HashMap<&str, usize> =HashMap::new();
-    for i in 0..rule_list.len() {
-        rule_map.insert(rule_list[i].start_queue.as_str(), i);
-    }
-
-    let mut accept_list:Vec<usize> = Vec::new();
-    let mut reject_list:Vec<usize> = Vec::new();
-    for part_idx in 0..parts_list.len() {
-        let mut current_queue = "in";
-        while current_queue != "A" && current_queue != "R" {
-
-        }
-        if current_queue == "A" {
-            accept_list.push(part_idx);
-        } else {
-            reject_list.push(part_idx);
-        }
-    }
-
-    let mut answer:i64=0;
-    for a_idx in 0..accept_list.len() {
-        let part = parts_list[accept_list[a_idx]];
-        let part_total = part.x + part.m + part.a + part.s;
-        println!("part: {:?} gives {}", part, part_total);
-        answer += part_total;
-    }
-
-
-
-    return answer.to_string();
-}
 
 fn parse_line(input_line: &String) -> Rule {
     let (s_queue, rest) = input_line.split_once("{").unwrap();
@@ -173,7 +126,7 @@ fn parse_line(input_line: &String) -> Rule {
         let comparsion = rp.as_bytes()[1] as char;
         let num_c_queue  = &rp[2..];
         let (num,n_queue) = num_c_queue.split_once(":").unwrap();
-        let z:i32 = num.parse().unwrap();
+        let z:i64 = num.parse().unwrap();
         let new_rule_part = RulePart {
             xmas_letter: letter,
             op: comparsion,
@@ -210,6 +163,91 @@ fn parse_xmas(line: &String) -> Part {
     p
 }
 
+
+
+fn part1(input_file: &str) -> String {
+    let lines = advent_2023::file_to_lines(input_file);
+
+
+    let mut rule_list:Vec<Rule> = Vec::new();
+    let mut index: usize = 0;
+
+
+    while lines[index] != "" {
+        let new_rule:Rule = parse_line(&lines[index]);
+        rule_list.push(new_rule);
+        index += 1;
+    }
+    index += 1;
+    let mut parts_list: Vec<Part> = Vec::new();
+
+
+    while index < lines.len() {
+        let p = parse_xmas(&lines[index]);
+        parts_list.push(p);
+        index += 1;
+    }
+    let parts_list = parts_list;
+
+    let mut rule_map: HashMap<&str, usize> =HashMap::new();
+    for i in 0..rule_list.len() {
+        rule_map.insert(rule_list[i].start_queue.as_str(), i);
+    }
+
+    let mut accept_list:Vec<usize> = Vec::new();
+    let mut reject_list:Vec<usize> = Vec::new();
+    for part_idx in 0..parts_list.len() {
+        let mut current_queue = "in";
+        let part = parts_list[part_idx];
+        println!("Processing Part:\t {}", part);
+        while current_queue != "A" && current_queue != "R" {
+            println!("current_queue: {}", current_queue);
+            let rule_for_q = &rule_list[rule_map[current_queue]];
+            println!("rule: {}", rule_for_q);
+            let mut all_rules_passed = true ;
+            for r in &rule_for_q.compares {
+                let v = match r.xmas_letter {
+                    'x' => {part.x},
+                    'm' => {part.m},
+                    'a' => {part.a}
+                    's' => {part.s},
+                    ch => {panic!("being asked to compared against value of {}", ch)}
+                };
+
+                if ((r.op == '<') && (v < r.num)) ||((r.op == '>') && (v > r.num)) {
+                    current_queue = r.queue_name.as_str();
+                    println!("\t {} {} {} : true  -> {}", v, r.op, r.num, current_queue);
+                    all_rules_passed = false;
+                    break;
+                }
+            }
+            if all_rules_passed {
+                current_queue = rule_for_q.last_queue.as_str();
+                println!("pass all tests, going to queue: {}", current_queue);
+            }
+        }
+        if current_queue == "A" {
+
+            accept_list.push(part_idx);
+            println!("placing part# {} in accept queue (now size: {})", part_idx, accept_list.len());
+        } else {
+            reject_list.push(part_idx);
+            println!("placing part# {} in reject queue (now size: {})", part_idx, reject_list.len());
+        }
+    }
+
+    let mut answer:i64=0;
+    for a_idx in 0..accept_list.len() {
+        let part = parts_list[accept_list[a_idx]];
+        let part_total = part.x + part.m + part.a + part.s;
+        println!("part: {:?} gives {}", part, part_total);
+        answer += part_total;
+    }
+
+
+
+    return answer.to_string();
+}
 
 fn part2(input_file: &str) -> String {
     let lines = advent_2023::file_to_lines(input_file);
