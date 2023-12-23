893d18e1ba8a386f99718804e614087b7d281d5b
dnabre <dnabre@happybunnylabs.com>
2023-12-23 08:51:43 -0700

    day 20
    parsing down
    everything should be wired up

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@
 #![allow(unreachable_code)]
 
 use std::collections::HashMap;
+use std::sync::atomic::AtomicU64;
+use std::sync::atomic::Ordering::SeqCst;
 use std::time::Instant;
-
 
 /*
     Advent of Code 2023: Day 20
@@ -50,15 +51,34 @@ fn main() {
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum Pulse {
     High,
-    Low
+    Low,
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct PulseInstance {
     strength: Pulse,
     source: usize,
-    dest: usize
+    dest: usize,
 }
+
+static HIGH_PULSE_COUNT: AtomicU64 = AtomicU64::new(0);
+static LOW_PULSE_COUNT: AtomicU64 = AtomicU64::new(0);
+
+impl PulseInstance {
+    fn new(strength: Pulse, source: usize, dest: usize) -> Self {
+        match strength {
+            Pulse::High => {
+                HIGH_PULSE_COUNT.fetch_add(1, SeqCst);
+            }
+            Pulse::Low => {
+                LOW_PULSE_COUNT.fetch_add(1, SeqCst);
+            }
+        }
+        Self { strength, source, dest }
+    }
+}
+
+
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 enum Module {
     FlipFlop {
@@ -66,119 +86,149 @@ enum Module {
         output: Vec<usize>,
     },
     Conjunction {
-        state: Vec<bool>,
+        state: Vec<Pulse>,
         inputs: Vec<usize>,
-        output: Vec<usize>
+        output: Vec<usize>,
     },
     Broadcaster {
         output: Vec<usize>,
-    }
+    },
 }
 
-const FLIP_FLOP_PREFIX: u8 =37_u8;
-const CONJ_PREFIX: u8= 38u8;
-const BROADCAST_PREFIX:u8=98_u8;
+const FLIP_FLOP_PREFIX: u8 = 37_u8;
+const CONJ_PREFIX: u8 = 38u8;
+const BROADCAST_PREFIX: u8 = 98_u8;
 
 fn part1(input_file: &str) -> String {
     let lines = advent_2023::file_to_lines(input_file);
 
-    let mut modules:Vec<Module>  = Vec::new();
-    let mut name_lookup:HashMap<&str, usize> = HashMap::new();
-    let mut name_by_id:HashMap<usize,&str> = HashMap::new();
+    let mut modules: Vec<Module> = Vec::new();
+    let mut name_lookup: HashMap<&str, usize> = HashMap::new();
+    let mut name_by_id: HashMap<usize, &str> = HashMap::new();
+    let mut module_names: Vec<&str> = Vec::new();
+    let mut output_modules: Vec<(&str, usize)> = Vec::new();
 
-    let mut m_broadcast_id:Option<usize> = None;
+    let mut m_broadcast_id: Option<usize> = None;
 
-
-
-
-
-
-    let mut id:usize = 0;
+    let mut assign_id = 0;
     for l in &lines {
-        println!("input: {}", l);
         let (src, dest) = l.split_once("->").unwrap();
-        println!("\t src: {}, dest: {}", src, dest);
+        let mut src = trim_module_prefix(src.trim());
+        module_names.push(src);
+        name_by_id.insert(assign_id, src);
+        name_lookup.insert(src, assign_id);
+        assign_id += 1;
+    }
+
+    let mut conj_list: Vec<usize> = Vec::new();
+    let mut id: usize = 0;
+    for l in &lines {
+        let (src, dest) = l.split_once("->").unwrap();
         let src = src.trim();
-        println!("\t src: {src}");
-        let dest_list:Vec<&str> = dest.split(",").map(|s| s.trim()).collect();
-        println!("\t dest: {} ", advent_2023::list_displayables_to_string(&dest_list));
-        let src_id = add_if_new(&mut name_lookup,&mut id, src  );
-        name_by_id.insert(src_id, src);
-        let mut dest_ids:Vec<usize> = Vec::new();
+        let dest_list: Vec<&str> = dest.split(",").map(|s| s.trim()).collect();
+
+        let src_id = name_lookup[trim_module_prefix(src)];
+
+        let mut dest_ids: Vec<usize> = Vec::new();
         for i in 0..dest_list.len() {
-            let dest_id = add_if_new(&mut name_lookup, &mut id, dest_list[i]);
-            name_by_id.insert(dest_id,dest_list[i]);
+            let key = dest_list[i];
+
+            if !name_lookup.contains_key(&key) {
+                let n_dest_id = assign_id;
+                assign_id += 1;
+
+                name_lookup.insert(key, n_dest_id);
+                name_by_id.insert(n_dest_id, key);
+                module_names.push(key);
+
+                output_modules.push((key, n_dest_id));
+            }
+            let dest_id = name_lookup[dest_list[i]];
             dest_ids.push(dest_id);
         }
-
         let f = src.as_bytes()[0];
-        println!("\t src.as_bytes[0] : {}", f);
-
-        let mut m = match f {
+        let m = match f {
             FLIP_FLOP_PREFIX => {
-                Module::FlipFlop {on: false, output: dest_ids}
-            },
+                Module::FlipFlop { on: false, output: dest_ids }
+            }
             CONJ_PREFIX => {
+                conj_list.push(src_id);
                 Module::Conjunction {
                     state: vec![],
                     inputs: vec![],
                     output: dest_ids,
                 }
-            },
+            }
             BROADCAST_PREFIX => {
                 let t = Module::Broadcaster { output: dest_ids };
-                m_broadcast_id= Some(src_id);
+                m_broadcast_id = Some(src_id);
                 t
-            },
-            _ => {panic!("unable to parse module: {}", f)}
+            }
+            _ => {
+                panic!("No Untyped Modules should be sources: {}", l);
+            }
         };
-        println!("module: {src_id:3}  :{:?}", m);
         modules.push(m);
     }
+    let mut broadcast_id;
+    if let Some(b) = m_broadcast_id {
+        broadcast_id = b;
+    } else {
+        panic!("didn't find broadcast id");
+    }
+    let broadcast_id = broadcast_id;
 
-    println!(" ------------------------------ ");
+    println!("    ---------------------------------------------");
+    println!("broadcast: {}", broadcast_id);
+    println!("output sinks: {:?}", output_modules);
+    println!("    ---------------------------------------------");
+
+
     for i in 0..modules.len() {
-        let name = name_by_id[&i];
-        let id = name_lookup[&name];
-        println!("{i:3} -> \t ({id:3},{name}) \t{:?}", modules[i]);
-    }
-
-
-
-
-
-    let answer = 0;
-    return answer.to_string();
-}
-
-fn add_if_new<'a>(map: &mut HashMap<&'a str, usize>, id_num: &mut usize, value: &'a str) -> usize  {
-    let current = map.get(&value);
-    let and_prefix = value.strip_prefix("&");
-    let pre_prefix = value.strip_prefix("%");
-    let mut new_value = value;
-    if let Some(s) = and_prefix {
-        new_value = s;
-    } else if let Some(s) = pre_prefix {
-        new_value = s;
-    }
-
-    let value = new_value;
-
-
-    match current {
-        None => {
-            let new_id = *id_num;
-            *id_num += 1;
-            map.insert(value, new_id) ;
-   //         println!("assign id: {} to {}", new_id, value);
-            return new_id;
-        }
-        Some(id) => {
-            return *id;
+        let mut m = &modules[i];
+        if let Module::Conjunction { state: ref states, inputs: ref ins, output: ref outs } = m {
+            assert_eq!(true, ins.is_empty());
+            assert_eq!(true, states.is_empty());
+            let mut new_states = Vec::new();
+            let mut new_inputs = Vec::new();
+            for j in 0..modules.len() {
+                if i == j { continue; }
+                match &modules[j] {
+                    Module::FlipFlop { on, output } => {
+                        if output.contains(&i) {
+                            new_inputs.push(j);
+                            new_states.push(Pulse::Low);
+                        }
+                    }
+                    Module::Conjunction { state, inputs, output } => {
+                        if output.contains(&i) {
+                            new_inputs.push(j);
+                            new_states.push(Pulse::Low);
+                        }
+                    }
+                    Module::Broadcaster { output } => {
+                        if output.contains(&i) {
+                            new_inputs.push(j);
+                            new_states.push(Pulse::Low);
+                        }
+                    }
+                }
+            }
+            let new_conj = Module::Conjunction {
+                state: new_states,
+                inputs: new_inputs,
+                output: outs.clone(),
+            };
+            modules[i] = new_conj;
         }
     }
-}
 
+
+
+
+
+
+let answer = 0; return answer.to_string(); }
 
 fn part2(input_file: &str) -> String {
     let lines = advent_2023::file_to_lines(input_file);
@@ -186,4 +236,15 @@ fn part2(input_file: &str) -> String {
 
     let answer = 0;
     return answer.to_string();
+}
+
+fn trim_module_prefix(i_src: &str) -> &str {
+    let mut src = i_src;
+    if i_src.starts_with("%") {
+        src = i_src.strip_prefix("%").unwrap();
+    }
+    if i_src.starts_with("&") {
+        src = i_src.strip_prefix("&").unwrap();
+    }
+    src
 }
