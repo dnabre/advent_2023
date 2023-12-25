ea99a1e12be43c32c1d7a817e0ffbe7164e87b5b
dnabre <dnabre@happybunnylabs.com>
2023-12-25 08:50:42 -0700

    quick and dirty karger

--- src/main.rs
+++ src/main.rs
@@ -60,9 +60,9 @@ impl Node {
     }
 }
 
-impl Node   {
-
-}
+type Graph = (V,E);
+type V = HashSet<String>;
+type E = Vec<(String,String)>;
 
 
 
@@ -70,45 +70,27 @@ impl Node   {
 fn part1(input_file: &str) -> String {
     let lines = advent_2023::file_to_lines(input_file);
 
-    let mut id_lookup:HashMap<&str,usize> = HashMap::new();
-    let mut name_list:Vec<&str> = Vec::new();
-    let mut labeled:HashSet<&str> = HashSet::new();
-    let mut node_list:Vec<Node> = Vec::new();
+    let (vertices,edges) = parse_input(&lines);
+
+    let mut rng = rand::thread_rng();
+
+    loop {
+        let mut vertices = vertices.clone();
+        let mut edges = edges.clone();
+        while vertices.len() > 2 {
+            let i = rng.gen_range(0..edges.len());
+            let (v1,v2) = edges[i].clone();
+
+            edges.swap_remove(i);
+            vertices.remove(&v1);
+            vertices.remove(&v2);
 
 
-    let mut lefts:Vec<&str> = Vec::new();
-    let mut part_id =0;
-    for l in &lines {
-        let (left,right) = l.split_once(":").unwrap();
-        let  mut r_list:Vec<&str> = right.split_whitespace().collect();
-        r_list.push(left);
 
-        for name in r_list.iter() {
-            if !labeled.contains(name) {
-                id_lookup.insert(name, part_id);
-                name_list.push(name);
-                labeled.insert(name);
-                let n = Node { id: part_id, adjacent: BTreeSet::new() };
-                node_list.push(n);
-                part_id += 1;
-            }
+
         }
     }
-    let node_count = node_list.len();
-    println!("id_lookup: {:?}", id_lookup);
 
-    for l in &lines {
-        let (left,right) = l.split_once(":").unwrap();
-        let  r_list:Vec<&str> = right.split_whitespace().collect();
-        let left_id = id_lookup[left];
-            for right in r_list {
-                let right_id = id_lookup[right];
-                node_list[left_id].add_edge_to(right_id);
-                node_list[right_id].add_edge_to(left_id);
-            }
-    }
-
-    let mut map_graph:HashMap<usize,Vec<usize>> = convert_adj_list_to_adj_map(&node_list);
 
 
 
@@ -117,7 +99,22 @@ fn part1(input_file: &str) -> String {
 }
 
 
+fn parse_input(lines:&Vec<String>)->Graph {
+    let n = lines.len();
+    let mut vertices:HashSet<String> = HashSet::with_capacity(n);
+    let mut edges:Vec<(String,String)> = Vec::with_capacity(n*n);
 
+    for i in 0..n {
+        let line = &lines[i];
+        let (left,right_side) = line.split_once(":").unwrap();
+        vertices.insert(left.to_string());
+        for right in right_side.split_whitespace() {
+            vertices.insert(right.to_string());
+            edges.push((left.to_string(),right.to_string()));
+        }
+    }
+    return (vertices, edges);
+}
 
 
 fn convert_adj_list_to_adj_map(node_list: &Vec<Node>) -> HashMap<usize, Vec<usize>> {
@@ -138,30 +135,4 @@ fn part2(input_file: &str) -> String {
 
     let answer = 0;
     return answer.to_string();
-}
-
-
-
-fn karger_algorithm(graph: &mut HashMap<usize, Vec<usize>>) -> usize {
-    while graph.len() > 2 {
-        let keys: Vec<_> = graph.keys().cloned().collect();
-        let u = rand::thread_rng().gen_range(0..keys.len());
-        let u_key = keys[u];
-        let v = rand::thread_rng().gen_range(0..graph[&u_key].len());
-        let w_key = graph[&u_key][v];
-
-        let w_key_values = graph[&w_key].clone();
-        graph.get_mut(&u_key).unwrap().extend(w_key_values.clone());
-        println!("w_key_values: {:?}", w_key_values);
-        for node in w_key_values.iter() {
-            let m_node_list = graph.get_mut(node);
-            if let Some(node_list) = m_node_list {
-                node_list.retain(|x| *x != w_key);
-                node_list.push(u_key);
-            }
-        }
-        graph.remove(&w_key);
-    }
-    graph.values().next().unwrap().len()
-
 }
