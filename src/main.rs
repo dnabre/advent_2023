af7be8ac98b4dfc53a44f84aaf5611fecc197c65
dnabre <dnabre@happybunnylabs.com>
2023-12-25 04:39:01 -0700

    day 22 ugly and slow but works

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,14 @@
-#![allow(unused_variables)]
-#![allow(unused_imports)]
-#![allow(unused_mut)]
-#![allow(dead_code)]
-#![allow(unused_assignments)]
-#![allow(unreachable_code)]
-
-
-use std::collections::{BTreeMap, HashMap};
-use std::fmt::Display;
+use std::collections::HashMap;
 use std::hash::Hash;
 use std::time::Instant;
 
 /*
     Advent of Code 2023: Day 22
-        part1 answer:
-        part2 answer:
+        part1 answer:   485
+        part2 answer:   74594
 
 */
-
-const ANSWER: (&str, &str) = ("825896364", "243566897206981");
-
+const ANSWER: (&str, &str) = ("485", "74594");
 
 fn main() {
     let _filename_test = "data/day22/test_input_01.txt";
@@ -28,10 +17,10 @@ fn main() {
     let filename_part1 = "data/day22/part1_input.txt";
     let filename_part2 = "data/day22/part2_input.txt";
 
-    // println!("Advent of Code, Day 22");
+    println!("Advent of Code, Day 22");
     println!("    ---------------------------------------------");
     let start1 = Instant::now();
-    let answer1 = part1(_filename_test);
+    let answer1 = part1(filename_part1);
     let duration1 = start1.elapsed();
 
     println!("\t Part 1: {:14} time: {:?}", answer1, duration1);
@@ -43,10 +32,10 @@ fn main() {
     let answer2 = part2(filename_part2);
     let duration2 = start2.elapsed();
 
-    // println!("\t Part 2: {:14} time: {:?}", answer2, duration2);
-    // if ANSWER.1 != answer2 {
-    //     println!("\t\t ERROR: Answer is WRONG. Got: {}, Expected {}", answer2, ANSWER.1);
-    // }
+    println!("\t Part 2: {:14} time: {:?}", answer2, duration2);
+    if ANSWER.1 != answer2 {
+        println!("\t\t ERROR: Answer is WRONG. Got: {}, Expected {}", answer2, ANSWER.1);
+    }
     println!("    ---------------------------------------------");
 }
 
@@ -57,30 +46,188 @@ struct Coord {
     z: i32,
 }
 
-#[derive(Debug, Clone, Copy)]
+fn parse_coords_pair(line: &String) -> (Coord, Coord) {
+    let (left, right) = line.split_once("~").unwrap();
+    let (cc1, cc2) = (parse_coords(left), parse_coords(right));
+    return (cc1, cc2);
+}
+
+
+fn parse_coords(l: &str) -> Coord {
+    let p1: Vec<&str> = l.split(",").collect();
+    let cc1 = Coord {
+        x: p1[0].parse().unwrap(),
+        y: p1[1].parse().unwrap(),
+        z: p1[2].parse().unwrap(),
+    };
+    return cc1;
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 struct Brick {
     id: usize,
     coords1: Coord,
     coords2: Coord,
 }
 
-impl Brick {
-    fn move_by(&self, delta: [i32; 3]) -> Brick {
-        let [dx, dy, dz] = delta;
 
-        Self {
-            id: self.id,
-            coords1: Coord { x: self.coords1.x + dx, y: self.coords1.y + dy, z: self.coords1.z + dz },
-            coords2: Coord { x: self.coords2.x + dx, y: self.coords2.y + dy, z: self.coords2.z + dz },
-        }
+fn parse_brick_list(lines:Vec<String>)->Vec<Brick> {
+    let mut brick_list = Vec::new();
+    for i in 0..lines.len() {
+        let l = &lines[i];
+        let (cc1, cc2) = parse_coords_pair(l);
+
+        let b = Brick {
+            id: i,
+            coords1: cc1,
+            coords2: cc2,
+        };
+        brick_list.push(b);
     }
+    brick_list.sort_by_cached_key(|b| b.coords1.z.min(b.coords2.z));
+    return brick_list;
 }
 
-impl Brick {
-    fn new(id: usize, coords1: Coord, coords2: Coord) -> Self {
-        Self { id, coords1, coords2 }
+
+fn part1(input_file: &str) -> String {
+    let lines = advent_2023::file_to_lines(input_file);
+    let mut bricks = parse_brick_list(lines);
+
+    let mut change = true;
+    while change {
+        let mut new_bricks = Vec::with_capacity(bricks.len());
+        change = false;
+        for brick in &bricks {
+            let new_brick = if brick.get_height_above_ground() > 1 {
+                let potential_brick = brick.move_by(0, 0, -1);
+                let mut intersects = false;
+                for other_brick in bricks.iter().filter(|b| b.id != brick.id) {
+                    if potential_brick.intersects(other_brick) {
+                        intersects = true;
+                        break;
+                    }
+                }
+                if intersects {
+                    brick.clone()
+                } else {
+                    change = true;
+                    potential_brick
+                }
+            } else {
+                brick.clone()
+            };
+            new_bricks.push(new_brick);
+        }
+        bricks = new_bricks;
+    }
+    let (supported_by,supports) = generate_supports(&mut bricks);
+
+
+// TODO break this chain out to something more readable
+    let answer = (0..bricks.len())
+        .filter(|i| supports.get(i)
+            .map_or(true, |s| s.iter().all(|j| supported_by[j].len() > 1))).count();
+    return answer.to_string();
+}
+
+
+
+
+
+fn part2(input_file: &str) -> String {
+    let lines = advent_2023::file_to_lines(input_file);
+    let mut bricks = Vec::new();
+    for i in 0..lines.len() {
+        let l = &lines[i];
+        let (cc1, cc2) = parse_coords_pair(l);
+
+        let b = Brick {
+            id: i,
+            coords1: cc1,
+            coords2: cc2,
+        };
+        bricks.push(b);
+    }
+    bricks.sort_by_cached_key(|b| b.coords1.z.min(b.coords2.z));
+
+
+
+    let mut change = true;
+    while change {
+        let mut new_bricks = Vec::with_capacity(bricks.len());
+        change = false;
+        for brick in &bricks {
+            let new_brick = if brick.get_height_above_ground() > 1 {
+                let potential_brick = brick.move_by(0, 0, -1);
+                let mut intersects = false;
+                for other_brick in bricks.iter().filter(|b| b.id != brick.id) {
+                    if potential_brick.intersects(other_brick) {
+                        intersects = true;
+                        break;
+                    }
+                }
+                if intersects {
+                    brick.clone()
+                } else {
+                    change = true;
+                    potential_brick
+                }
+            } else {
+                brick.clone()
+            };
+            new_bricks.push(new_brick);
+        }
+        bricks = new_bricks;
     }
 
+    let (supported_by,_) = generate_supports(&mut bricks);
+
+    let mut answer = 0;
+    for brick in &bricks {
+        let mut supported_by_temp = supported_by.clone();
+        // remove current brick from supported_by
+        supported_by_temp.iter_mut().for_each(|(_, l)| l.retain(|i| *i != brick.id));
+        let mut change = true;
+        while change {
+            change = false;
+            // Check if any bricks are now unsupported and remove them from supports
+            let unsupported = supported_by_temp.iter().filter(|(_, l)| l.is_empty()).map(|(i, _)| *i).collect::<Vec<_>>();
+            for brick_id in &unsupported {
+                supported_by_temp.iter_mut().for_each(|l| l.1.retain(|i| *i != *brick_id));
+                supported_by_temp.remove(&brick_id);
+                change = true;
+            }
+            answer += unsupported.len();
+        }
+    }
+
+    return answer.to_string();
+}
+
+fn generate_supports(bricks: &mut Vec<Brick>) -> (HashMap<usize, Vec<usize>>, HashMap<usize, Vec<usize>>) {
+    let mut supported_by: HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut supports: HashMap<usize, Vec<usize>> = HashMap::new();
+
+    for (i, brick) in bricks.iter().enumerate() {
+        let down_brick = brick.move_by(0, 0, -1);
+        for (j, other_brick) in bricks.iter().enumerate() {
+            if i != j && down_brick.intersects(other_brick) {
+                supported_by.entry(i).or_default().push(j);
+                supports.entry(j).or_default().push(i);
+            }
+        }
+    }
+
+    for (i, brick) in bricks.iter().enumerate() {
+        if brick.get_height_above_ground() == 1 {
+            supported_by.entry(i).or_default().push(usize::MAX);
+        }
+    }
+    (supported_by,supports)
+}
+
+
+impl Brick {
     fn intersects(&self, other: &Self) -> bool {
         let (x1, y1, z1) = (self.coords1.x, self.coords1.y, self.coords1.z);
         let (x2, y2, z2) = (self.coords2.x, self.coords2.y, self.coords2.z);
@@ -103,110 +250,28 @@ impl Brick {
         let zintersects = (otherlowz >= lowz && otherlowz <= highz) || (otherhighz >= lowz && otherhighz <= highz) || (lowz >= otherlowz && lowz <= otherhighz) || (highz >= otherlowz && highz <= otherhighz);
         xintersects && yintersects && zintersects
     }
+
+    fn move_by(&self, x: i32, y: i32, z: i32) -> Self {
+        let (x1, y1, z1) = (self.coords1.x, self.coords1.y, self.coords1.z);
+        let (x2, y2, z2) = (self.coords2.x, self.coords2.y, self.coords2.z);
+        Self {
+            id: self.id,
+            coords1: Coord {
+                x: x1 + x,
+                y: y1 + y,
+                z: z1 + z,
+            },
+            coords2: Coord {
+                x: x2 + x,
+                y: y2 + y,
+                z: z2 + z,
+            },
+        }
+    }
+
     fn get_height_above_ground(&self) -> i32 {
-        let z1 = self.coords1.z;
-        let z2 = self.coords2.z;
-        return z1.min(z2);
+        let (_, _, z1) = (self.coords1.x, self.coords1.y, self.coords1.z);
+        let (_, _, z2) = (self.coords2.x, self.coords2.y, self.coords2.z);
+        z1.min(z2)
     }
-}
-
-
-
-
-fn part1(input_file: &str) -> String {
-    let lines = advent_2023::file_to_lines(input_file);
-
-    let mut bricks = lines.iter().filter(|line| !line.is_empty())
-        .map(|line| line.split_once("~").unwrap())
-        .enumerate()
-        .map(|(id, (coords1, coords2))| Brick::new(id, parse_coords(coords1), parse_coords(coords2)))
-        .collect::<Vec<_>>();
-    for i in 0..bricks.len() {
-        println!("_ {i:3}= {:?}", bricks[i]);
-    }
-
-return String::new();
-
-    let mut brick_list: Vec<Brick> = Vec::new();
-
-    // for i in 0..lines.len() {
-    //     let (p1, p2) = parse_coord_pair(&lines[i]);
-    //     let b = Brick::new(i, p1, p2);
-    //     brick_list.push(b);
-    // }
-
-    let brick_list = drop_bricks(brick_list);
-    let mut supported_by: HashMap<usize, Vec<usize>> = HashMap::new();
-    let mut supports: HashMap<usize, Vec<usize>> = HashMap::new();
-
-    for (i,brick) in brick_list.iter().enumerate() {
-        let down_brick = brick.move_by([0, 0, -1]);
-        if brick.get_height_above_ground() == 1 {
-            supported_by.entry(i).or_default().push(usize::MAX)
-        }
-    }
-    let mut answer = 0u64;
-    for i in 0..brick_list.len() {
-        let b = brick_list[i];
-        let v = &supported_by[&i];
-        println!("brick[{i:3}]: {:?} -> {:?}", b, v);
-
-        if v.len() == 1 { answer += 1; }
-    }
-
-
-    return answer.to_string();
-}
-
-fn drop_bricks(bricks: Vec<Brick>) -> Vec<Brick> {
-    println!("dropping");
-    let mut bricks = bricks;
-    let mut change = true;
-    while change {
-        let mut new_bricks = Vec::with_capacity(bricks.len());
-        change = false;
-        for brick in &bricks {
-            let new_brick = if brick.get_height_above_ground() > 1 {
-                let potential_brick = brick.move_by([0, 0, -1]);
-                let mut intersects = false;
-                for other_brick in bricks.iter().filter(|b| b.id != brick.id) {
-                    if potential_brick.intersects(other_brick) {
-                        intersects = true;
-                        break;
-                    }
-                }
-                if intersects {
-                    brick.clone()
-                } else {
-                    change = true;
-                    potential_brick
-                }
-            } else {
-                brick.clone()
-            }
-                ;
-            new_bricks.push(new_brick);
-        }
-        bricks = new_bricks;
-    }
-    println!("down dropping");
-    return bricks;
-}
-
-fn parse_coords(l: &str) -> Coord {
-    let p1: Vec<&str> = l.split(",").collect();
-    let cc1 = Coord {
-        x: p1[0].parse().unwrap(),
-        y: p1[1].parse().unwrap(),
-        z: p1[2].parse().unwrap(),
-    };
-    return cc1;
-}
-
-fn part2(input_file: &str) -> String {
-    let lines = advent_2023::file_to_lines(input_file);
-
-
-    let answer = 0;
-    return answer.to_string();
 }
